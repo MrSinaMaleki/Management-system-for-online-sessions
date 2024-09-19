bafd7c6e825e9efce6a8faedcbe1418e94c7d465
MrSinaMaleki <sina67631@gmail.com>
2024-09-19 16:14:16 +0330

    Class Based Views

--- online_classes/urls.py
+++ online_classes/urls.py
@@ -3,10 +3,19 @@ from online_classes import views
 
 
 urlpatterns = [
-    path('', views.all_sessions, name="home"),
-    path('add_session', views.create_session, name="create_session"),
-    path('edit_session/<int:pk>', views.edit_session, name="edit_session"),
-    path('detail_session/<int:pk>', views.detail_session, name="detail_session"),
-    path('delete_session/<int:pk>', views.delete_session, name="delete_session"),
+    # path('', views.all_sessions, name="home"),
+    path('', views.AllSessions.as_view(), name='all-sessions'),
+
+
+    # path('detail_session/<int:pk>', views.detail_session, name="detail_session"),
+    path('detail_session/<int:pk>', views.SessionDetailView.as_view(), name="detail_session"),
+
+    # path('add_session', views.create_session, name="create_session"),
+    path('add_session', views.CreateSession.as_view(), name="create_session"),
+    # path('edit_session/<int:pk>', views.edit_session, name="edit_session"),
+    path('edit_session/<int:pk>', views.EditSession.as_view(), name="edit_session"),
+
+    # path('delete_session/<int:pk>', views.delete_session, name="delete_session"),
+    path('delete_session/<int:pk>', views.DeleteSession.as_view(), name="delete_session"),
 
 ]
--- online_classes/views.py
+++ online_classes/views.py
@@ -1,58 +1,101 @@
-from datetime import datetime
-
-from django.shortcuts import render, get_object_or_404, redirect
-from online_classes.forms import SessionForm
+from django.shortcuts import render
 from online_classes.models import Session
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib import messages
 
 
 # Create your views here.
-def all_sessions(request):
-    error = None
-    if request.method == "POST":
+# def all_sessions(request):
+#     error = None
+#     if request.method == "POST":
+#
+#         if start_time and end_time and start_time < end_time:
+#             sessions = Session.objects.filter(end_time__lte=end_time, start_time__gte=start_time)
+#             return render(request, 'home.html', {"sessions": sessions})
+#         else:
+#             error = "please enter valid date "
+#
+#     return render(request, 'home.html', {"sessions": Session.objects.all(), "error": error})
+
+
+class AllSessions(ListView):
+    model = Session
+    template_name = 'home.html'
+    context_object_name = 'sessions'
+
+    def post(self, request, *args, **kwargs):
+        # context = super().get_queryset()
         start_time, end_time = request.POST["start"], request.POST["end"]
         if start_time and end_time and start_time < end_time:
-            sessions = Session.objects.filter(end_time__lte=end_time, start_time__gte=start_time)
-            return render(request, 'home.html', {"sessions": sessions})
-        else:
-            error = "please enter valid date "
+            sessions =  Session.objects.filter(end_time__lte=end_time, start_time__gte=start_time)
+            return render(request, self.template_name, {self.context_object_name: sessions})
 
-    return render(request, 'home.html', {"sessions": Session.objects.all(), "error": error})
-
-
-def create_session(request):
-    context = dict()
-
-    my_form = SessionForm(request.POST or None)
-    context["form"] = my_form
-
-    if request.method == 'POST':
-        if my_form.is_valid():
-            my_form.save()
-            print("Saved!")
-
-    return render(request, "session_form.html", context)
+        messages.add_message(request, messages.ERROR, "Invalid data.", extra_tags='alert alert-error')
+        return render(request, self.template_name, {})
 
 
-def edit_session(request, pk):
-    obj = get_object_or_404(Session, pk=pk)
-    # context = {}
+# def detail_session(request, pk):
+#     obj = get_object_or_404(Session, pk=pk)
+#     return render(request, "detail_session.html", {"session": obj})
 
-    if request.method == "POST":
-        my_form = SessionForm(request.POST, instance=obj)
-        if my_form.is_valid():
-            my_form.save()
-            return redirect("home")
-    return render(request, "session_form.html", {"form": SessionForm(instance=obj)})
+class SessionDetailView(DetailView):
+    model = Session
+    template_name = 'detail_session.html'
 
 
-def detail_session(request, pk):
-    obj = get_object_or_404(Session, pk=pk)
-    return render(request, "detail_session.html", {"session": obj})
+
+# def create_session(request):
+#     context = dict()
+#
+#     my_form = SessionForm(request.POST or None)
+#     context["form"] = my_form
+#
+#     if request.method == 'POST':
+#         if my_form.is_valid():
+#             my_form.save()
+#             print("Saved!")
+#
+#     return render(request, "session_form.html", context)
+
+class CreateSession(CreateView):
+    model = Session
+    fields = '__all__'
+    success_url = '/'
+    template_name = 'session_form.html'
 
 
-def delete_session(request, pk):
-    obj = get_object_or_404(Session, pk=pk)
-    if request.method == "POST":
-        obj.delete()
-        return redirect("home")
-    return render(request, "confirm.html", {"session": obj})
+# def edit_session(request, pk):
+#     obj = get_object_or_404(Session, pk=pk)
+#     # context = {}
+#
+#     if request.method == "POST":
+#         my_form = SessionForm(request.POST, instance=obj)
+#         if my_form.is_valid():
+#             my_form.save()
+#             return redirect("home")
+#     return render(request, "session_form.html", {"form": SessionForm(instance=obj)})
+
+class EditSession(UpdateView):
+    model = Session
+    fields = '__all__'
+    success_url = '/'
+    template_name = 'session_form.html'
+
+
+
+# def delete_session(request, pk):
+#     obj = get_object_or_404(Session, pk=pk)
+#     if request.method == "POST":
+#         obj.delete()
+#         return redirect("all-sessions")
+#     return render(request, "confirm.html", {"session": obj})
+
+class DeleteSession(DeleteView):
+    model = Session
+    success_url = '/'
+    template_name = 'confirm.html'
+
+
+
+
